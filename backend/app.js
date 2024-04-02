2a0a1b043ae06548b7244584684ff41a7720129c
nitin-jaswal24 <nitin0970.be21@chitkara.edu.in>
2024-04-02 17:24:15 +0530

    cors headers set

--- backend/app.js
+++ backend/app.js
@@ -40,11 +40,15 @@ import multer from 'multer'
 // })
 
 
-app.use(cors({
-    origin:'*' ,
-    methods:['GET','POST','DELETE','PUT'],
-    credentials:true
-}))
+app.use(cors())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
